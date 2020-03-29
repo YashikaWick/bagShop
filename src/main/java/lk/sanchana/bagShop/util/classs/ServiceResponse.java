436322e5eac865b6f4ea5ac960a905c4e3cd05d2
Lositha <asakahatapitiya@gmail.com>
2020-03-29 22:35:47 +0530

    Initial commit

---
+++ src/main/java/lk/sanchana/bagShop/util/classs/ServiceResponse.java
@@ -0,0 +1,15 @@
+package lk.sanchana.bagShop.util.classs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceResponse<T> {
+
+    private String status;
+    private T data;
+
+}
---
+++ src/test/java/lk/sanchana/bagShop/BagShopApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sanchana.bagShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
