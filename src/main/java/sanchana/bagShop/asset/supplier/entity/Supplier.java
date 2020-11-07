1ff90b621da81f70d04ad5c516a6eff98d0a6806
Sandi1994 <sadanimalsh@gmail.com>
2020-11-07 18:45:46 +0530

    Supplier

--- src/main/java/sanchana/bagShop/asset/supplier/entity/Supplier.java
+++ src/main/java/sanchana/bagShop/asset/supplier/entity/Supplier.java
@@ -30,9 +30,6 @@ public class Supplier extends AuditEntity {
     @Column( nullable = false, unique = true )
     private String code;
 
-    @Size( min = 2, message = "Your BRN cannot be accepted" )
-    private String brn;
-
     @Size( max = 10, min = 9, message = "Mobile number length should be contained 10 and 9" )
     private String contactOne;
 
