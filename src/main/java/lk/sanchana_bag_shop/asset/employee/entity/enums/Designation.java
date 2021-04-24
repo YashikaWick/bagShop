f241ae82124a1103b7d3d50087fe1129669c2633
Sandi1994 <sadanimalsh@gmail.com>
2021-04-24 21:20:08 +0530

    Merge branch 'master' of https://github.com/LalithK90/bagShop
    # Conflicts:
    #	src/main/java/lk/sanchana_bag_shop/configuration/SecurityConfig.java

--- src/main/java/lk/sanchana_bag_shop/asset/employee/entity/enums/Designation.java
+++ src/main/java/lk/sanchana_bag_shop/asset/employee/entity/enums/Designation.java
@@ -6,11 +6,16 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Designation {
-  ADMIN("ADMIN"),
-  CASHIER("CASHIER"),
-  SK("STOCK KEEPER"),
-  MANAGER("MANAGER"),
-  ACC("ACCOUNTANT");
+  ADMIN("Admin"),
+  MANAGER("Manager"),
+  PROCUREMENT_MANAGER("Procurement Manager"),
+  HR_MANAGER("HR Manager"),
+  ACCOUNT_MANAGER("Account Manager"),
+  CASHIER("Cashier");
 
   private final String designation;
+
+//sir thawa podi ekak mata methana designation change karann beri weid?
+//  hr manager and procurment mager ain krl single manager kenek hari tiyala stock keeper kenek danna beri weida?
+
 }
